23d10cb30085c8a01c450b8d6dcfb6afaedaac86
Angelo Barboni <a.barboni16@imperial.ac.uk>
2021-03-15 16:26:32 +0000

    handling empty vectors

--- src/Observers/DLuenberger.jl
+++ src/Observers/DLuenberger.jl
@@ -24,14 +24,22 @@ function setInitialState(o::DLUE{V}, z0::Vector{V}) where {V<:Real}
     setfield!(o, :z, z0)
 end
 
-function setNeighbours(o::DLUE{V}, neighIdx::Vector{UInt}, neighVals::Vector{Matrix{V}}) where {V} 
+function setNeighbours(o::DLUE{V}, neighIdx::AbstractVector, neighVals::AbstractVector{Matrix{V}}) where {V} 
+    if isempty(neighIdx) || isempty(neighVals) 
+        neighIdx = UInt[]
+        neighVals = Vector{Matrix{V}}()
+    end
     setfield!(o, :neighbours, neighIdx)
-    setfield!(o, :Aij, neighVals)
+    setfield!(o, :Aij, copy(neighVals))
     setfield!(o, :E, isempty(o.Aij) ? Matrix{V}(undef, o.nx, 0) : foldl(hcat, o.Aij))
 end
 
-function updateState(o::DLUE{V}, u::Vector{V}, y::Vector{V}, hatbarx::Vector{V}) where {V<:Real}
-    setfield!(o, :z, o.F*o.z + o.B*u + o.L*y + o.E*hatbarx)
+function updateState(o::DLUE{V}, u::Vector{V}, y::Vector{V}, hatbarx::AbstractVector{V}) where {V<:Real}
+    if isempty(hatbarx)
+        setfield!(o, :z, o.F*o.z + o.B*u + o.L*y)
+    else
+        setfield!(o, :z, o.F*o.z + o.B*u + o.L*y + o.E*hatbarx)
+    end
 end
 
 updateState(o::DLUE{V}, u::V, y::Vector{V}, hatbarx::Vector{V}) where {V} = updateState(o, [u], y, hatbarx)
@@ -40,7 +48,7 @@ outputMap(o::DLUE{<:Real}) = o.C*o.z
 
 function Base.getproperty(o::DLUE, s::Symbol) 
     if s === :nx
-        return size(F,1)
+        return size(o.F, 1)
     else
         getfield(o, s)
     end
